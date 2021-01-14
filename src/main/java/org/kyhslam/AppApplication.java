575828e1308529d6ae951042d2af22cbbbf893b7
kyhslam <kyhslam@gmail.com>
2021-01-14 18:19:52 +0900

    'init'

--- src/main/java/org/kyhslam/AppApplication.java
+++ src/main/java/org/kyhslam/AppApplication.java
@@ -1,2 +1,12 @@
-package org.kyhslam;public class AppApplication {
+package org.kyhslam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
 }
